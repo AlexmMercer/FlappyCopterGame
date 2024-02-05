6f716d2a4adcb0c8fc0c923a1035939eef60b37c
Alex Mercer <120363131+AlexmMercer@users.noreply.github.com>
2024-02-05 13:43:31 +0300

    C

--- Assets/Scripts/GeneralScriptsForMainGameplay/ObstacleGeneration.cs
+++ Assets/Scripts/GeneralScriptsForMainGameplay/ObstacleGeneration.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 public class ObstacleGeneration : MonoBehaviour
 {
     [SerializeField] float SendTimer = 1;
-    [SerializeField] float Frequency = 2;
+    [SerializeField] float FrequencyLowerBound = 1.7f;
+    [SerializeField] float FrequencyUpperBound = 2.5f;
+    private float Frequency;
+    private float speedCoefficient = 1.0f;
     [SerializeField] float Position;
     [SerializeField] GameObject Obstacle;
+    [SerializeField] private GameObject[] obstaclePrefabs;
     [SerializeField] GameObject BarrelPrefab;
 
     private float obstaclesXPosition = 2.234f;
@@ -19,26 +23,34 @@ public class ObstacleGeneration : MonoBehaviour
 
     private void Start()
     {
+
     }
     private void Update()
     {
-        SendTimer -= Time.deltaTime;
+        Frequency = Random.Range(FrequencyLowerBound, FrequencyUpperBound);
+        SendTimer -= speedCoefficient * Time.deltaTime;
         if (SendTimer <= 0)
         {
+            var obstacleIndex = (int)Random.Range(0, obstaclePrefabs.Length);
+            var obstacle = obstaclePrefabs[obstacleIndex];
             Position = Random.Range(obstaclesHeightLowerValue, obstaclesHieghtUpperValue);
             transform.position = new Vector3(obstaclesXPosition,
                                              Position, obstaclesZPosition);
-            int generateBarrel = Random.Range(0, 2);
-            if (generateBarrel == 0)
+            if(obstacleIndex == 0 || obstacleIndex == 1)
             {
-                Obstacle.transform.Find("Barrel").gameObject.SetActive(false);
+                int generateBarrel = Random.Range(0, 2);
+                if (generateBarrel == 0)
+                {
+                    obstacle.transform.Find("Barrel").gameObject.SetActive(false);
+                }
+                else
+                {
+                    obstacle.transform.Find("Barrel").gameObject.SetActive(true);
+                }
             }
-            else
-            {
-                Obstacle.transform.Find("Barrel").gameObject.SetActive(true);
-            }
-            Instantiate(Obstacle, transform.position, transform.rotation);
+            Instantiate(obstacle, transform.position, transform.rotation);
             SendTimer = Frequency;
+            speedCoefficient += 0.01f;
         }
     }
 }
